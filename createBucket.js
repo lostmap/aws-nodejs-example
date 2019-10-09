6ad2aaff0654d2b52f11d385b93428be19642bdb
okabanov <omkabano@mts.ru>
2019-10-09 16:02:54 +0300

    change win

--- createBucket.js
+++ createBucket.js
@@ -1,19 +1,17 @@
 var AWS = require("aws-sdk");
 var fs = require('fs');
 var https = require('https');
-var certs = [
-    fs.readFileSync('MTS Root CA.pem'),
-];
 
 var config = {
+    accessKeyId: "10f777f4b1d0400485ecfc55148fc0de",
+    secretAccessKey: "efcabac998d142df952d5c024956b084",
     endpoint: "https://sds.mts.ru",
     sslEnabled: true,
     s3ForcePathStyle: true,
     region: 'RegionOne',
     httpOptions: {
         agent: new https.Agent({
-            rejectUnauthorized: false,
-            ca: certs
+            rejectUnauthorized: false
         })
     }
 };
